9f3e40d284bc575b35ad6997546e2d7a3c63112d
laeticiamouto <laeticiamouto.2@gmail.com>
2024-06-26 20:59:26 -0400

    Update backend

--- config/database.js
+++ config/database.js
@@ -11,11 +11,26 @@ export const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.M
 
 const initDb = async () => {
     try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
+      // await sequelize.authenticate();
+      // console.log('Connection has been established successfully.');
   
-      await sequelize.sync({ force: true });
-      console.log('All models were synchronized successfully.');
+      // await sequelize.sync({ force: true });
+      // console.log('All models were synchronized successfully.');
+      // Synchronisation des tables dans l'ordre approprié
+        sequelize.authenticate().then(() => {
+          User = require('./models/User')(sequelize, Sequelize);
+          Recipe = require('./models/Recipe')(sequelize, Sequelize);
+
+          // Sync User table first
+          return User.sync();
+        }).then(() => {
+          // Then sync Recipe table which references User
+          return Recipe.sync();
+        }).then(() => {
+          console.log('Tables synchronized successfully');
+        }).catch(err => {
+          console.error('Error synchronizing tables:', err);
+        });
     } catch (error) {
       console.error('Unable to connect to the database:', error);
     }
